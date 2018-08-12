74c7525c87f28ef56babb2a4f7ce812bba865a98
Spectre <antoine@doussaud.org>
2018-08-11 20:17:49 -0700

    Create update_pi.sh

---
+++ update_pi.sh
@@ -0,0 +1,2 @@
+cp ~/plantoids_music/copyToRaspberryPi/copy_to_supercolliderFiles/* ~/prynth/server/public/supercolliderfiles/
+cp ~/plantoids_music/copyToRaspberryPi/copy_to_extensions/* /usr/local/share/SuperCollider/Extensions/
